cdf537ad7a6df61ebc9e5828512c1be5d9ef1943
Michael Provan <michael@mprovan.com>
2017-04-03 22:47:04 -0400

    Added existing files
    Moved files to GitHub repository

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿function openTab(evt, tabName) {
+    var i, x, tabLink;
+    x = document.getElementsByClassName("mp-tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tabLink = document.getElementsByClassName("tablink");
+    for (i = 0; i < tabLink.length; i++) {
+        tabLink[i].className = tabLink[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+}
