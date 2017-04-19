ab79e6f973b992f090408bec981c5b10c3b72ffa
Michael Provan <michael@mprovan.com>
2017-04-18 21:52:13 -0400

    Redesign + Adventure Game Started

---
+++ sfc/cop2822/adv_engine.js
@@ -0,0 +1,48 @@
+﻿var moves = 0,
+    score = 0;
+
+function start() {
+    setInterval(update, 500);
+}
+
+function update() {
+    flash(document.getElementById("cursor"));
+}
+
+function flash(x) {
+    if (x.innerHTML == "&nbsp;")
+        x.innerHTML = "_";
+    else
+        x.innerHTML = "&nbsp;";
+}
+
+function getKey(e) {
+    var keynum;
+    var input = document.getElementById("input");
+
+    if (window.event) { // IE                    
+        keynum = e.keyCode;
+    } else if (e.which) { // Netscape/Firefox/Opera                   
+        keynum = e.which;
+    }
+
+    if (keynum == 8 || keynum == 46) {  //If Backspace or DEL is pressed, remove last character inputted
+        input.innerText = input.innerText.replace(/(\s+)?.$/, "");
+    }
+    else if (keynum == 32)  //If space is pressed, add a space
+        input.innerText += " ";
+    else if (keynum == 13) {    //If enter is pressed, send the input, then clear box
+        document.getElementById("game").innerHTML += "<p>>" + input.innerText + "</p>";
+        submitCommand();
+        input.innerText = "";
+    }
+    else if (keynum >= 65 && keynum <= 90)  //letters are added to box
+        input.innerText += String.fromCharCode(keynum);
+    else
+        return false;   //everything else returns false
+}
+
+function submitCommand() {
+    moves++;
+
+}
---
+++ sfc/cop2822/adv_objects.js
@@ -0,0 +1,14 @@
+﻿var player = {
+    damage: 
+}
+
+function Room() {
+    this.name = "Dark Room";
+    this.desc = "";
+    this.items = [];
+}
+
+function Item() {
+    this.name = "";
+    this.desc = "";
+}
