27bf77ecc2b472b86283962fed14ed3ff767d0bc
Michael Provan <michael@mprovan.com>
2022-06-30 23:06:27 -0400

    Buncha Changes
    Mostly functional. Needs some style and optimization. Undo button needed for purchases, too.

---
+++ classes.js
@@ -0,0 +1,243 @@
+class Tech {
+    constructor(t){
+        this.id = t.id;
+        this.name = t.name;
+        this.desc = t.description;
+        this.type = t.type;
+        this.exp = t.expansion;
+        this.total = t.quantity;
+        this.qty = 0;
+        this.bought = 0;
+        
+        this.isNext = 0;
+        
+        this.div = document.createElement("div");
+        this.divIMG = document.createElement("img");
+        this.divBadge = document.createElement("i");
+        this.divNext = document.createElement("i");
+        this.div.appendChild(this.divIMG);
+        this.div.appendChild(this.divBadge);
+        this.div.appendChild(this.divNext);
+        this.updateDiv();
+    }
+    get quantity(){
+        let q = this.qty - this.bought;
+        return q;
+    }
+    
+    get remaining(){
+        let r = this.total - this.qty - this.isNext;
+        return r;
+    }
+    
+    get imageFile(){
+        if (this.quantity <= 0){return "images/".concat(this.id,".png");}
+        return "images/active/".concat(this.id,".png");
+    }
+    //get div(){return this.updateDiv();}
+    
+    get typeColor(){
+        switch(this.type){
+            case "Military":
+                return "#7b4f50";
+            case "Grid":
+                return "#5c716c";
+            case "Nano":
+                return "#836d46";
+            case "Rare":
+                return "#7e718c";
+            default:
+                return "DimGrey";
+        }
+    }
+    
+    updateDiv(){
+        //let div = document.createElement("div");
+        this.div.setAttribute("class","w3-round-large w3-col s1 w3-display-container tech");
+        this.div.setAttribute("id",this.id);
+        this.div.onclick = function(){
+            if(confirm("Buy this tech?")){
+                buyTech(this.id);
+            }
+            return 1;
+        }
+        
+        //let divIMG = document.createElement("img");
+        this.divIMG.setAttribute("class","w3-image");
+        this.divIMG.setAttribute("src",this.imageFile);
+        //this.divIMG.setAttribute("onclick","");
+        
+        //let divBadge = document.createElement("i");
+        this.divBadge.setAttribute("class","fa-solid fa-".concat(this.quantity," w3-display-bottomleft w3-margin techBadge"));
+        
+        this.divNext.setAttribute("class","fa-solid fa-hourglass w3-display-bottomright w3-margin");
+        
+        if (this.quantity >= 1){
+            this.div.style.display = 'block';
+            this.div.style.backgroundImage = 'linear-gradient(LightGrey,'.concat(this.typeColor,')');
+            //this.div.style.outline = '2px solid LightGreen';
+            this.divBadge.style.display = 'block';
+        } else {
+            this.div.style.backgroundImage = 'linear-gradient(DimGrey,'.concat(this.typeColor,')');
+            this.divBadge.style.display = 'none';
+        }
+        
+        if (this.isNext > 0){
+            this.divNext.style.display = 'block';
+        } else {this.divNext.style.display = 'none';}
+        
+    }
+    
+    draw(n){this.qty += n;this.updateDiv();console.log("drawing",this.name);}
+    buy(){this.bought += 1;this.updateDiv();console.log("bought",this.name);}
+    predict(n){this.isNext += n;this.updateDiv();console.log("predicting",this.name);}
+}
+
+class TechManager {
+    constructor(id,techbox,players,options){
+        this.id = id;
+        this.techbox = techbox;
+        this.types = ["Military","Grid","Nano","Rare"];
+        this.div = document.getElementById(this.id);
+        this.techBag = [];
+        this.playerCount = players;
+        this.options = options;
+        
+        this.nextButton = document.getElementById("predict");
+        
+        this.setup(this.playerCount,this.options);
+        
+    }
+    
+    get drawCount(){
+        switch (this.playerCount){
+            case 2:
+                return [12,4];
+            case 3:
+                return [14,6];
+            case 4:
+                return [16,7];
+            case 5:
+                return [18,8];
+            case 6:
+                return [20,9];
+            case 7:
+                return [22,10];
+            case 8:
+                return [24,11];
+            case 9:
+                return [26,11];
+            default:
+                return [12,4];
+        }
+    }
+    
+    makeRow(type){
+        let row = document.createElement("div");
+        row.setAttribute("class","w3-row");
+        row.setAttribute("id",type);
+        //console.log(type);
+        
+        return row;
+    }
+    
+    setup(p,o){
+        for (let i = 0, l = this.types.length; i<l; i++){
+            this.div.appendChild(this.makeRow(this.types[i]));
+        }
+        
+        this.techBag = this.fillBag(o);
+        this.makeStandardTray();
+    }
+    
+    fillBag(o){
+        let is2Ebalance = o[0];
+        let isRotA = o[1];
+        let isSotR = o[2];
+        let isTractorBeam = o[3];
+        let is2ENew = o[4];
+        let is2EDev = o[5];
+        
+        var techArray = [];
+        
+        for (let i = 0, l = this.techbox.length; i<l; i++){
+            //console.log(techbox[i].id,techbox[i].type);
+            if (this.techbox[i].expansion == "1E S" && is2Ebalance){console.log("Skip 1eb");continue;}
+            if (this.techbox[i].expansion == "2E S" && !is2Ebalance){console.log("Skip 2eb");continue;}
+            if (this.techbox[i].expansion == "RotA" && !isRotA){console.log("Skip rota");continue;}
+            if (this.techbox[i].expansion == "SotF" && !isSotR){console.log("Skip sotr");continue;}
+            if (this.techbox[i].expansion == "SP1" && !isTractorBeam){console.log("Skip tb");continue;}
+            if (this.techbox[i].expansion == "2E T" && !is2ENew){console.log("Skip 2en");continue;}
+            if (this.techbox[i].expansion == "2E D" && !is2EDev){console.log("Skip 2ed");continue;}
+            
+            let newTech = new Tech(this.techbox[i]);
+            techArray.push(newTech);
+        }
+        
+        return techArray;
+    }
+    
+    makeStandardTray(){
+        
+        for (let i = 0, l = this.techBag.length; i<l; i++){
+            //console.log(techbox[i].id,techbox[i].type);
+            if (this.techBag[i].type == "Rare"){
+                this.techBag[i].div.style.display = "none";
+            }
+            
+            document.getElementById(this.techBag[i].type).appendChild(this.techBag[i].div);
+            
+        }
+    }
+    
+    drawTech(){
+        //console.log("drawing tech")
+        var bag = this.techBag;
+        
+        for (var i = 0; i < bag.length; i++) {
+            var tech = bag[i];
+            if (tech.isNext > 0){
+                let n = tech.isNext;
+                tech.isNext = 0;
+                tech.draw(n);
+            }
+        }
+    }
+    
+    predictTech(n){
+        //console.log("drawing tech")
+        if (this.options[6]){this.drawTech()}
+        
+        for (var i = 0; i < n; i++){
+            let randTech = this.getRandomTech();
+            randTech.predict(1);
+        }
+        
+        if (!this.options[6]){this.drawTech()}
+    }
+    
+    getRandomTech(){
+        var bag = this.techBag;
+        var total = 0;
+        for (var i = 0; i < bag.length; i++){total += bag[i].remaining}
+        
+        var rand = Math.random()*total;
+        for (var i = 0; i < bag.length; i++) {
+            var tech = bag[i];
+            if (rand < tech.remaining){
+                return tech;
+            }
+            rand -= tech.remaining;
+        }
+        
+    }
+    
+    buyTech(id){
+        var bag = this.techBag;
+        
+        for (var i = 0; i < bag.length; i++){
+            var tech = bag[i];
+            if (tech.id == id){return tech.buy();}
+        }
+    }
+}
--- scripts.js
+++ scripts.js
@@ -4,12 +4,16 @@ var techcsv = `id,name,quantity,description,type,expansion
 2,Plasma Cannon,5,You may Upgrade Plasma Cannon Ship Parts.,Military,1E
 3,Phase Shield,4,You may Upgrade Phase Shield Ship Parts.,Military,1E
 4,Advanced Mining,4,You may place Population Cubes in the Advanced Materials squares with your Colony Ships.,Military,1E
-5,Tachyon Source,3,You may UpgradeTachyon Source Ship Parts.,Military,1E
-6,Plasma Missile,3,You may Upgrade Plasma Missile Ship Parts.,Military,1E
-7,Gluon Computer,3,You may Upgrade Gluon Computer Ship Parts.,Military,1E
+5,Tachyon Source,3,You may Upgrade Tachyon Source Ship Parts.,Military,1E
+6,Plasma Missile,3,You may Upgrade Plasma Missile Ship Parts.,Military,1E S
+7,Gluon Computer,3,You may Upgrade Gluon Computer Ship Parts.,Military,1E S
+46,Gluon Computer (2e),3,You may Upgrade Gluon Computer Ship Parts.,Military,2E S
+45,Plasma Missile (2e),3,You may Upgrade Plasma Missile Ship Parts.,Military,2E S
 8,Gauss Shield,5,You may Upgrade Gauss Dhield Ship Parts.,Grid,1E
-9,Improved Hull,5,You may Upgrade Improved Hull Ship Parts.,Grid,1E
-10,Fusion Source,5,You may Upgrade Fusion Source Ship Parts.,Grid,1E
+9,Improved Hull,5,You may Upgrade Improved Hull Ship Parts.,Grid,1E S
+10,Fusion Source,5,You may Upgrade Fusion Source Ship Parts.,Grid,1E S
+47,Fusion Source (2e),5,You may Upgrade Fusion Source Ship Parts.,Grid,2E S
+48,Improved Hull (2e),5,You may Upgrade Improved Hull Ship Parts.,Grid,2E S
 11,Positron Computer,4,You may Upgrade Positron Computer Ship Parts.,Grid,1E
 12,Advanced Economy,4,You may place Population Cubes in the Advanced Money squares with your Colony Ships.,Grid,1E
 13,Tachyon Drive,3,You may Upgrade Tachyon Drive Ship Parts.,Grid,1E
@@ -17,12 +21,16 @@ var techcsv = `id,name,quantity,description,type,expansion
 15,Quantum Grid,3,"You receive two additional Influence Discs, placed immediately on your Influence Track.",Grid,1E
 16,Nanorobots,5,You may Build one additional Ship or Structure.,Nano,1E
 17,Fusion Drive,5,You may Upgrade Fusion Drive Ship Parts.,Nano,1E
-18,Advanced Robotics,5,"You receive one additional Influence Disc, placed immediately on your Influence Track.",Nano,1E
-19,Orbital,4,You may Build Orbitals.,Nano,1E
+18,Advanced Robotics,5,"You receive one additional Influence Disc, placed immediately on your Influence Track.",Nano,1E S
+19,Orbital,4,You may Build Orbitals.,Nano,1E S
+50,Orbital (2e),4,You may Build Orbitals.,Nano,2E S
+49,Advanced Robotics (2e),5,"You receive one additional Influence Disc, placed immediately on your Influence Track.",Nano,2E S
 20,Advanced Labs,4,You may place Population Cubes in the Advanced Science squares with your Colony Ships.,Nano,1E
 21,Monolith,3,You may Build Monoliths.,Nano,1E
-22,Artifact Key,3,You must take 5 Resources of one type for each Artifact on your hexes.,Nano,1E
-23,Wormhole Generator,3,"You may Explore, Influence and Move through a hex edge that has a Wormhole on just one side.",Nano,1E
+22,Artifact Key,3,You must take 5 Resources of one type for each Artifact on your hexes.,Nano,1E S
+23,Wormhole Generator,3,"You may Explore, Influence and Move through a hex edge that has a Wormhole on just one side.",Nano,1E S
+51,Wormhole Generator (2e),3,"You may Explore, Influence and Move through a hex edge that has a Wormhole on just one side.",Nano,2E S
+52,Artifact Key (2e),3,You must take 5 Resources of one type for each Artifact on your hexes.,Nano,2E S
 24,Antimatter Splitter,1,Allows you to split damage from Antimatter Cannons over several targets.,Rare,RotA
 25,Neutron Absorber,1,Enemy Neutron Bombs have no effect.,Rare,RotA
 26,Distortion Sheild,1,Enemy missiles get a –2 modifier to hit.,Rare,RotA
@@ -40,23 +48,123 @@ var techcsv = `id,name,quantity,description,type,expansion
 38,Transition Drive,1,Allows you to take Transition Drive Ship Parts.,Rare,SotF
 39,Absorption Shield,1,Allows you to take Absorption Shield Ship Parts.,Rare,SotF
 40,Tractor Beam,1,Your opponents cannot retreat from a battle against you.,Rare,SP1
-41,Improved Logistics,1,Gain 1 additional Move Activation during each Move Action you take.,Rare,2E
-42,Pico Modulator,1,Gain 2 additional Upgrade Activations during each Upgrade Action you take.,Rare,2E
-43,Warp Portal (Tech),1,Immediately place the Warp Portal Tile on any of your controlled hexes. The Warp Portal connects to all other Warp Portals and is worth 1VP at the end of the game if controlled.,Rare,2E
-44,Ancient Labs (Tech),1,Immediately draw one Discovery Tile.,Rare,2E
-45,Plasma Missile (2e),3,You may Upgrade Plasma Missile Ship Parts.,Military,2E
-46,Gluon Computer (2e),3,You may Upgrade Gluon Computer Ship Parts.,Military,2E
-47,Fusion Source (2e),5,You may Upgrade Fusion Source Ship Parts.,Grid,2E
-48,Improved Hull (2e),5,You may Upgrade Improved Hull Ship Parts.,Grid,2E
-49,Advanced Robotics (2e),5,"You receive one additional Influence Disc, placed immediately on your Influence Track.",Nano,2E
-50,Orbital (2e),4,You may Build Orbitals.,Nano,2E
-51,Wormhole Generator (2e),3,"You may Explore, Influence and Move through a hex edge that has a Wormhole on just one side.",Nano,2E
-52,Artifact Key (2e),3,You must take 5 Resources of one type for each Artifact on your hexes.,Nano,2E`
+41,Improved Logistics,1,Gain 1 additional Move Activation during each Move Action you take.,Rare,2E T
+42,Pico Modulator,1,Gain 2 additional Upgrade Activations during each Upgrade Action you take.,Rare,2E T
+43,Warp Portal (Tech),1,Immediately place the Warp Portal Tile on any of your controlled hexes. The Warp Portal connects to all other Warp Portals and is worth 1VP at the end of the game if controlled.,Rare,2E D
+44,Ancient Labs (Tech),1,Immediately draw one Discovery Tile.,Rare,2E D`
 
-var techbox = $.csv.toObjects(techcsv);
+var techObjects = $.csv.toObjects(techcsv);
+var TechBoard;
+
+var p = 5;
+var o = [0,0,0,0,0,0,0]
+
+function loadTechs(){
+    TechBoard = new TechManager("TechBoard",techObjects,p,o);
+    TechBoard.predictTech(TechBoard.drawCount[0]);
+    //TechBoard.predictTech(TechBoard.drawCount[1]);
+}
+
+function buyTech(id){
+    var bag = TechBoard.techBag;
+    for (var i = 0; i< bag.length; i++){
+        if (bag[i].id == id && bag[i].qty > bag[i].bought){
+            console.log("buying",bag[i].name);
+            bag[i].buy();
+            
+        }
+    }
+}
+
+var slider = document.getElementById("playerCount");
+var output = document.getElementById("playerCountOutput");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = p = this.value;
+}
+
+var drawButton = document.getElementById("draw");
+
+drawButton.onclick = function(){
+    if(confirm("Draw new tiles?")){
+        TechBoard.predictTech(TechBoard.drawCount[1]);
+    }
+    return 1;
+}
+
+var startButton = document.getElementById("startButton");
+var startMenu = document.getElementById("StartMenu");
+
+startButton.onclick = function(){
+    if(confirm("Start the Game with these settings?")){
+        startMenu.style.display = "none";
+        loadTechs();
+    }
+    return 1;
+}
+
+var checkbox0 = document.getElementById("2E S");
+var checkbox1 = document.getElementById("RotA");
+var checkbox2 = document.getElementById("SotR");
+var checkbox3 = document.getElementById("SP1");
+var checkbox4 = document.getElementById("2E T");
+var checkbox5 = document.getElementById("2E D");
+var checkbox6 = document.getElementById("PT");
+
+checkbox0.oninput = function(){
+    if(this.checked){
+        o[0] = this.value
+    } else {o[0] = 0}
+    console.log(o[0]);
+}
+
+checkbox1.oninput = function(){
+    if(this.checked){
+        o[1] = this.value
+    } else {o[1] = 0}
+    console.log(o[1]);
+}
+
+checkbox2.oninput = function(){
+    if(this.checked){
+        o[2] = this.value
+    } else {o[2] = 0}
+    console.log(o[2]);
+}
+
+checkbox3.oninput = function(){
+    if(this.checked){
+        o[3] = this.value
+    } else {o[3] = 0}
+    console.log(o[3]);
+}
+
+checkbox4.oninput = function(){
+    if(this.checked){
+        o[4] = this.value
+    } else {o[4] = 0}
+    console.log(o[4]);
+}
+
+checkbox5.oninput = function(){
+    if(this.checked){
+        o[5] = this.value
+    } else {o[5] = 0}
+    console.log(o[5]);
+}
+
+checkbox6.oninput = function(){
+    if(this.checked){
+        o[6] = this.value
+    } else {o[6] = 0}
+    console.log(o[6]);
+}
 
 
 
+/*
 function loadTechs(){
     for (let i = 0, l = techbox.length; i<l; i++){
             //console.log(techbox[i].id,techbox[i].type);
@@ -74,8 +182,9 @@ function addTechSlot(tech){
     //console.log(id.concat(" ",type));
     
     const techDiv = document.createElement("div");
-    techDiv.setAttribute("class",type.concat(" w3-round-large w3-col s1 w3-display-container tech"));
+    techDiv.setAttribute("class","w3-round-large w3-col s1 w3-display-container tech");
     techDiv.setAttribute("status","inactive");
+    if (type == "Rare") {techDiv.style.display = "none";}
     
     const techImg = document.createElement("img");
     techImg.setAttribute("src","images/".concat(id,".png"));
@@ -112,4 +221,4 @@ function makeInactive(id) {
   tech.src = "images/".concat(id,".png");
   tech.setAttribute("onclick","makeActive(".concat(id,")"));
   tech.parentElement.setAttribute("status","inactive");
-}
+}*/
